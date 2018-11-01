e5af9a4173649321c9de139b3365c5f6ac09139b
MicaelLeal <mycaellmoura@hotmail.com>
2018-11-01 16:09:51 -0300

    Atualizando

--- connectedin/perfis/models.py
+++ connectedin/perfis/models.py
@@ -1,45 +1,6 @@
 from django.db import models
 
-
 class User(models.Model):
     email = models.CharField(max_length=60)
     password = models.CharField(max_length=20)
     dt_birth = models.DateField()
-
-
-class Profile(models.Model):
-    name = models.CharField(max_length=60)
-    user = models.ForeignKey(User,
-                             on_delete=models.CASCADE,
-                             related_name='profiles')
-
-
-class Post(models.Model):
-    text = models.CharField(max_length=60)
-    date = models.DateField()
-    profile = models.ForeignKey(Profile,
-                                on_delete=models.CASCADE,
-                                related_name='posts')
-
-
-class Comment(models.Model):
-    text = models.CharField(max_length=60)
-    date = models.DateField()
-    profile = models.ForeignKey(Profile,
-                                on_delete=models.CASCADE,
-                                related_name='comments')
-    post = models.ForeignKey(Post,
-                             on_delete=models.CASCADE,
-                             related_name='comments')
-
-
-class Message(models.Model):
-    tipo = models.CharField(max_length=60)
-    date = models.DateField()
-    post = models.ForeignKey(Post,
-                             on_delete=models.CASCADE,
-                             related_name='messages')
-    profile = models.ForeignKey(Profile,
-                                on_delete=models.CASCADE,
-                                related_name='messages')
-    weidht = models.IntegerField()
